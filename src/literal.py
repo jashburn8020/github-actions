f10b57e3ab00ff80c5e6ee724dabbebdcaa7d89e
jashburn <jashburn8020@gmail.com>
2022-06-29 17:07:01 +0100

    Replace python test files to use a dependency.

--- src/literal.py
+++
@@ -1,8 +0,0 @@
-"""Return some literal stuff."""
-
-from typing import Literal
-
-
-def give_me_five() -> Literal[5]:
-    """Return the number 5."""
-    return 5
--- tests/literal_test.py
+++
@@ -1,4 +0,0 @@
-import literal
-
-def test_give_me_five() -> None:
-    assert literal.give_me_five() == 5
